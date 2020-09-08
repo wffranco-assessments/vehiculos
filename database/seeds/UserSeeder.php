71b96a0e0b0c1a12ca9f6624631f86e99ef3110d
Willem Franco <willemfranco@gmail.com>
2020-09-08 11:43:26 -0500

    Añadido tipo de usuario administrador
    - agregado usuario admin

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+            'is_admin' => true,
+        ]);
+    }
+}
